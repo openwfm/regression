7341f381288da4f5876732ccf1523f2f52545825
Jan Mandel <jan.mandel@gmail.com>
2023-06-24 03:02:25 +0000

    ncdiff4

--- ncdiff4.py
+++ ncdiff4.py
@@ -1,7 +1,18 @@
 import netCDF4 as nc
 import numpy as np
+import sys
 
-def compare_netcdf_files(file1, file2):
+def ncdiff4(file1, file2, do_print=1):
+    ###
+    Compare two netcdf files
+    :param file1: file name
+    :param file2: file name
+    :param do_print: print if True
+    :returns: max relative error
+    ###
+
+    maxreldif = 0.
+
     # Open netCDF files
     dataset1 = nc.Dataset(file1)
     dataset2 = nc.Dataset(file2)
@@ -11,10 +22,17 @@ def compare_netcdf_files(file1, file2):
     times2 = dataset2.variables["Times"][:]
     
     # Iterate through all time steps in both files
+    rows1, cols1 = times1.shape
+    rows2, cols2 = times2.shape
+    if rows1 ~= rows2 or cols1 ~= cols2:
+        print('arrays Times are not the same size')
+        return np.inf
     for time_index, (time1, time2) in enumerate(zip(times1, times2)):
+        ntested = 0
         # Check if the times are equal
         if np.array_equal(time1, time2):
-            print(f"Time step {time_index}: {time1.tobytes().decode().strip()}")
+            if do_print>2:
+                print(f"Time step {time_index}: {time1.tobytes().decode().strip()}")
             
             # Compare variables
             for var_name, variable in dataset1.variables.items():
@@ -29,6 +47,7 @@ def compare_netcdf_files(file1, file2):
                     
                     # Check if the dimensions are equal (ignoring unlimited time dimension)
                     if var1.shape[1:] == var2.shape[1:]:
+                        ntested += 1
                         # Compute norms
                         max_norm1 = np.max(np.abs(var1[time_index]))
                         max_norm2 = np.max(np.abs(var2[time_index]))
@@ -36,26 +55,34 @@ def compare_netcdf_files(file1, file2):
                         two_norm2 = np.linalg.norm(var2[time_index])
 
                         # Compute relative differences
-                        rel_diff_max = abs(max_norm1 - max_norm2) / max(max_norm1, max_norm2, 1e-10)
-                        rel_diff_two = abs(two_norm1 - two_norm2) / max(two_norm1, two_norm2, 1e-10)
+                        rel_diff_max = abs(max_norm1 - max_norm2) / max(max_norm1, max_norm2, np.finfo(float).eps)
+                        rel_diff_two = abs(two_norm1 - two_norm2) / max(two_norm1, two_norm2, np.finfo(float).eps)
                         
-                        # Print the results
-                        print(f"Variable {var_name}:")
-                        print(f"  max-norm: {max_norm1}, {max_norm2} | Relative difference: {rel_diff_max}")
-                        print(f"  2-norm: {two_norm1}, {two_norm2} | Relative difference: {rel_diff_two}")
+                        maxreldif = max(maxreldif, rel_diff_max)
+                        
+                        if do_print > 2:
+                            # Print the results
+                            print(f"Variable {var_name}:")
+                            print(f"  max-norm: {max_norm1}, {max_norm2} | Relative difference: {rel_diff_max}")
+                            print(f"  2-norm: {two_norm1}, {two_norm2} | Relative difference: {rel_diff_two}")
                     else:
-                        print(f"Variable {var_name} dimensions don't match. Skipping.")
+                        print(f"Variable {var_name} dimensions don't match. Exiting.")
+                        return np.inf 
                 else:
-                    print(f"Variable {var_name} not present in both files. Skipping.")
+                    if do_print > 1:
+                        print(f"Variable {var_name} not present in both files. Skipping.")
 
         else:
-            print(f"Time step {time_index} skipped due to different times.")
+           if do_print >0:
+                print(f"Time step {time_index} skipped due to different times.")
 
     # Close datasets
     dataset1.close()
     dataset2.close()
 
-# Example usage
-file1 = 'path_to_your_first_netcdf_file'
-file2 = 'path_to_y
+    return maxreldif
+
+if __name__ == "__main__":
+    # Example usage
+    print('max relative difference',ncdiff4(sys.argv[1],sys.argv[2]),do_print=2)
 
